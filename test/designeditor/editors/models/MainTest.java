8e46902abadfea7f6bafb78cd0e456f9817b0ab4
LKG <LKG@LKG-PC>
2016-11-18 21:05:01 +0800

    更新

--- test/designeditor/editors/models/MainTest.java
+++ test/designeditor/editors/models/MainTest.java
@@ -1,5 +1,8 @@
 package designeditor.editors.models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -7,6 +10,7 @@ import javax.persistence.Persistence;
 public class MainTest {
 	private static final String PERSISTENCE_UNIT_NAME = "designeditor";
 	private static EntityManagerFactory factory;
+	private static SimpleDateFormat date = new SimpleDateFormat("yyyymmddhhmmssSSS");
 
 	public static void main(String[] args) {
 		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
@@ -16,7 +20,7 @@ public class MainTest {
 
 		for (int i = 0; i < 40; i++) {
 			Project project = new Project();
-			project.setProjectId("TEST_PROJECT_ID" + i);
+			project.setProjectId(date.format(new Date()) + i);
 			project.setProjectIdName("TEST_PROJECT_ID_NAME" + i);
 			em.persist(project);
 			em.persist(project);
